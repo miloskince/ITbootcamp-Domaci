b1d283b00f42459842b82ab17bd497fc9ee8ea5d
Miloš Pavlović <miloskince@gmail.com>
2019-11-10 00:08:36 +0100

    Prepravljen zadatak nakon Cvijanove zamerke;

--- Nedelja3/2 Dan/zadatak1.js
+++ Nedelja3/2 Dan/zadatak1.js
@@ -3,9 +3,9 @@ function max(a,b,c)
     if(a>b && a>c){return a};
     if(b>a && b>c){return b};
     if(c>a && c>b){return c};
-    if(a==b && b==c){return 'Brojevi "a" i "b" i "c" su iste vrednosti'};
-    if(a==b){return 'Brojevi "a" i "b" su maximum ali su iste vrednosi'};
-    if(a==c){return 'Brojevi "a" i "c" su maximum ali su iste vrednosi'};
-    if(b==c){return 'Brojevi "b" i "c" su maximum ali su iste vrednosi'};
+    if(a==b && b==c){return a || b || c};
+    if(a==b){return b || a};
+    if(a==c){return a || c};
+    if(b==c){return b || c};
 }
-console.log(max(5,10,20));
+console.log(max(10,20,30));
