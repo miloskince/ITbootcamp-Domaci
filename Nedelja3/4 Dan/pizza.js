4e30cdf4cb9491a39d4f9fea7c604c357858679c
Miloš Pavlović <miloskince@gmail.com>
2019-11-10 16:56:19 +0100

    Dodat zaboravljen datum u Dan 4 zadatku;

--- Nedelja3/4 Dan/pizza.js
+++ Nedelja3/4 Dan/pizza.js
@@ -36,10 +36,79 @@ for (let button of orderNowBtn)
 function broilo0(element)
 {   
     var dateTimeNow = new Date();
+    var day = dateTimeNow.getDay();
+    var danUmesecu = dateTimeNow.getDate();
+
+    switch(day){
+        case 0:
+            day='Nedelja';
+            break;
+        case 1:
+            day='Ponedeljak';
+            break;
+        case 2:
+            day='Utorak';
+            break;
+        case 3:
+            day='Sreda';
+            break;
+        case 4:
+            day='Cetvrtak';
+            break;
+        case 5:
+            day='Petak';
+            break;
+        case 6:
+            day='Subota';
+            break;
+    }
+
+    var month = dateTimeNow.getMonth();
+
+    switch(month){
+        case 0:
+            month='Januar';
+            break;
+        case 1:
+            month='Februar';
+            break;
+        case 2:
+            month='Mart';
+            break;
+        case 3:
+            month='April';
+            break;
+        case 4:
+            month='Maj';
+            break;
+        case 5:
+            month='Jun';
+            break;
+        case 6:
+            month='Jul';
+            break;
+        case 7:
+            month='Avgust';
+            break;
+        case 8:
+            month='Septembar';
+            break;
+        case 9:
+            month='Oktobar';
+            break;
+        case 10:
+            month='Novembar';
+            break;
+        case 11:
+            month='Decembar';
+            break;
+    }
+
+    var year = dateTimeNow.getFullYear();
     var hours = dateTimeNow.getHours();
     var minutes = dateTimeNow.getMinutes();
     var seconds = dateTimeNow.getSeconds();
-    var timeStampString = `${hours}:${minutes}:${seconds}`;
+    var timeStampString = `${day} ${danUmesecu}.${month}.${year}  ${hours}:${minutes}:${seconds}`;
     var paragraf1=`<p>NISTE NARUCILI NI 1 PIZZU!!! ${timeStampString}</p>`;
     var paragraf2=`<p>NARUCILI STE ${pizzaCount} PIZZA!!! ${timeStampString}</p>`;
     if(pizzaCount==0){poruka.innerHTML=paragraf1;}
